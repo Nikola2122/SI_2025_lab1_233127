5d23b745fbcf0bdad6247074114e401c909d7c6b
Nikola Iliev <nikolailiev074@gmail.com>
2025-03-18 22:37:15 +0100

    Added feature: remove-a-task-by-name

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -64,7 +64,14 @@ class TaskManager {
 
     // 1. Remove a task by name
     public void removeTask(String name) {
-        // TODO: Implement removal logic
+        Task t = null;
+        for (Task task : tasks) {
+            if (task.getName().equals(name)) {
+                t = task;
+                break;
+            }
+        }
+        this.tasks.remove(t);
     }
 
     // 2. Find all completed tasks
